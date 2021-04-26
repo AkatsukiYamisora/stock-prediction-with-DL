4f048015a9355785493cab3afa6de3f54b63d419
AkatsukiYamisora <1185933892@qq.com>
2021-04-27 01:03:59 +0800

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -7,37 +7,66 @@
 """
 import pandas as pd
 
-# 起止日期
-date = '2014-01-01', '2020-12-31'
-
 
 class Backtest:
-    def __init__(self, start_date=date[0], end_date=date[1], index=0):
+    def __init__(self, start_date='2014-01-01', end_date='2020-12-31', index=0, start_cash=100000):
         """
         设置起止时间
         index: 0:上证 50  1:沪深 300  2:中证 500
         """
         self.start_date = start_date
         self.end_date = end_date
-        self.stocks = 'sz50', 'hs300', 'zz500'
+        self.index_name = 'sz50', 'hs300', 'zz500'
         self.indexes = 'sh.000016', 'sh.000300', 'sh.000905'
-        self.stocks_codes = pd.read_csv('./data/{}_stocks.csv'.format(self.stocks[index]))
-        self.index = pd.read_csv('./data/stocks/{}.csv'.format(self.indexes[index]))
+        self.data_path = './data/stocks/'
+        self.stocks = pd.read_csv('./data/{}_stocks.csv'.format(self.index_name[index]))
+        self.stocks_codes = self.stocks['code']
+        self.index = pd.read_csv('{}{}.csv'.format(self.data_path, self.indexes[index]))
         self.trading_dates = self.index['date']
+        self.cash = start_cash
+
+    def buy(self, stock_codes, trading_date: str) -> bool:
+        """
+        买入trading date的所有stock codes股票
+        trading date为'yyyy-mm-dd'格式字符串
+        """
+        if stock_codes.empty:
+            return False
+        stocks = pd.DataFrame()
+        for stock_code in stock_codes:
+            data = pd.read_csv('{}{}.csv'.format(self.data_path, stock_code), index_col='date')
+            if trading_date in data.index:
+                stocks = stocks.append(data.loc[trading_date], ignore_index=True)
+        stocks = stocks.set_index('code')
+        print(stocks)
+        return True
+
+    def sell(self, stock_codes, trading_date: str) -> bool:
+        """
+        买入trading date的所有stock codes股票
+        trading date为'yyyy-mm-dd'格式字符串
+        """
+        if stock_codes.empty:
+            return False
+        stocks = pd.DataFrame()
+        for stock_code in stock_codes:
+            data = pd.read_csv('{}{}.csv'.format(self.data_path, stock_code), index_col='date')
+            if trading_date in data.index:
+                stocks = stocks.append(data.loc[trading_date], ignore_index=True)
+        stocks = stocks.set_index('code')
+        print(stocks)
+        return True
 
     def trade(self, trading_date: str):
         """
         一次交易日
         date为'yyyy-mm-dd'格式字符串
         """
-        for stock_code in self.stocks_codes['code']:
-            data = pd.read_csv('./data/stocks/{}.csv'.format(stock_code), index_col='date')
-            print(data)
-            print(data.index)
-            if trading_date in data.index:
-                print(data.loc[trading_date])
+        pass
 
 
 if __name__ == '__main__':
     bt = Backtest()
     bt.trade(bt.trading_dates[0])
+    b = bt.buy(bt.stocks_codes[:3], bt.trading_dates[0])
+    print(b)
