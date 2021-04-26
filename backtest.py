eaad4a8a5db75f6c3229721801b722b9f6b549a8
AkatsukiYamisora <1185933892@qq.com>
2021-04-26 21:45:07 +0800

    change data downloader, add part of backtest
    修改数据下载，增加单只股票下载，增加回测类部分内容

--- backtest.py
+++ backtest.py
@@ -7,6 +7,37 @@
 """
 import pandas as pd
 
-# 设置回测时间段
-start_date = '2015-01-01'
-end_date = '2020-12-31'
+# 起止日期
+date = '2014-01-01', '2020-12-31'
+
+
+class Backtest:
+    def __init__(self, start_date=date[0], end_date=date[1], index=0):
+        """
+        设置起止时间
+        index: 0:上证 50  1:沪深 300  2:中证 500
+        """
+        self.start_date = start_date
+        self.end_date = end_date
+        self.stocks = 'sz50', 'hs300', 'zz500'
+        self.indexes = 'sh.000016', 'sh.000300', 'sh.000905'
+        self.stocks_codes = pd.read_csv('./data/{}_stocks.csv'.format(self.stocks[index]))
+        self.index = pd.read_csv('./data/stocks/{}.csv'.format(self.indexes[index]))
+        self.trading_dates = self.index['date']
+
+    def trade(self, trading_date: str):
+        """
+        一次交易日
+        date为'yyyy-mm-dd'格式字符串
+        """
+        for stock_code in self.stocks_codes['code']:
+            data = pd.read_csv('./data/stocks/{}.csv'.format(stock_code), index_col='date')
+            print(data)
+            print(data.index)
+            if trading_date in data.index:
+                print(data.loc[trading_date])
+
+
+if __name__ == '__main__':
+    bt = Backtest()
+    bt.trade(bt.trading_dates[0])
--- data_downloader.py
+++ data_downloader.py
@@ -9,8 +9,16 @@ import baostock as bs
 import pandas as pd
 
 # 设置数据下载时间段
-start_date = '2015-01-01'
+start_date = '2014-01-01'
 end_date = '2020-12-31'
+stocks = 'sz50', 'hs300', 'zz500'
+indexes = 'sh.000016', 'sh.000300', 'sh.000905'
+data_path = './data/stocks/'
+# 设置是否下载数据
+download_stocks = True
+download_indexes = True
+# 设置单只股票下载 填入股票代码
+download_special = ''
 
 # 登陆系统
 lg = bs.login()
@@ -23,37 +31,56 @@ sz = bs.query_sz50_stocks()
 hs = bs.query_hs300_stocks()
 zz = bs.query_zz500_stocks()
 
-for rs, name in zip([sz, hs, zz], ['sz50', 'hs300', 'zz500']):
-    rs_stocks = []
-    while (rs.error_code == '0') & rs.next():
+
+def status(raw):
+    if raw.error_code != '0':
+        print('query_history_k_data_plus respond error_code:' + rs.error_code)
+        print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
+
+
+def data_save(raw, csv_name, path=data_path):
+    data = []
+    while (raw.error_code == '0') & raw.next():
         # 获取一条记录，将记录合并在一起
-        rs_stocks.append(rs.get_row_data())
-    result = pd.DataFrame(rs_stocks, columns=rs.fields)
+        data.append(raw.get_row_data())
+    df = pd.DataFrame(data, columns=raw.fields)
     # 结果集输出到csv文件
-    result.to_csv("./data/{}_stocks.csv".format(name), index=False)
-    print(result)
+    df.to_csv("{}{}.csv".format(path, csv_name), index=False)
+    print(df)
+    return df
 
-    for code in result['code']:
-        stock_data = []
-        print('Downloading ' + code)
-        rs = bs.query_history_k_data_plus(code,
-                                          "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,"
-                                          "tradestatus,pctChg,isST",
-                                          start_date=start_date, end_date=end_date,
-                                          frequency="d", adjustflag="3")
-        if rs.error_code != '0':
-            print('query_history_k_data_plus respond error_code:' + rs.error_code)
-            print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
 
-        # 打印结果集
-        while (rs.error_code == '0') & rs.next():
-            # 获取一条记录，将记录合并在一起
-            stock_data.append(rs.get_row_data())
-        result2 = pd.DataFrame(stock_data, columns=rs.fields)
+if download_stocks:
+    for rs, name in zip((sz, hs, zz), stocks):
+        # 获取股票名称与代码
+        result = data_save(rs, name+'_stocks', './data/')
 
-        # 结果集输出到csv文件
-        result2.to_csv("./data/stocks/{}.csv".format(code), index=False)
-        print(result2)
+        for code in result['code']:
+            stock_data = []
+            print('Downloading ' + code)
+            rs = bs.query_history_k_data_plus(code,
+                                              "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,"
+                                              "tradestatus,pctChg,isST",
+                                              start_date=start_date, end_date=end_date,
+                                              frequency="d", adjustflag="3")
+            status(rs)
+            data_save(rs, code)
+
+if download_indexes:
+    for index in indexes:
+        rs = bs.query_history_k_data_plus(index,
+                                          "date,code,open,high,low,close,preclose,volume,amount,pctChg",
+                                          start_date=start_date, end_date=end_date, frequency="d")
+        status(rs)
+        data_save(rs, index)
+
+if download_special:
+    rs = bs.query_history_k_data_plus(download_special,
+                                      "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,"
+                                      "tradestatus,pctChg,isST",
+                                      start_date=start_date, end_date=end_date, frequency="d")
+    status(rs)
+    data_save(rs, download_special)
 
 # 登出系统
 bs.logout()
