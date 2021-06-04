67500f47a348bba7fb7b65128673f4592750b643
AkatsukiYamisora <1185933892@qq.com>
2021-06-04 17:11:47 +0800

    Update prediction.py
    修改训练与预测函数结构，优化数据集生成效率，增加比例化处理，rnn扩展为4种分别训练预测

--- prediction.py
+++ prediction.py
@@ -36,8 +36,8 @@ class StockDataset(Dataset):
         self.stocks_codes = self.stocks['code']
         # 输入列
         self.input_columns = ('open', 'high', 'low', 'close', 'preclose', 'volume', 'amount',
-                              'turn', 'pctChg', 'peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ',
-                              'total_share', 'float_share', 'free_share', 'total_mv', 'circ_mv')
+                              'turn', 'peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ', 'total_share',
+                              'float_share', 'free_share', 'total_mv', 'circ_mv')
         # 数据集
         if not os.path.exists('{}{}.pkl'.format(self.base_data_path, self.index_name)):
             remake_data = True
@@ -48,24 +48,21 @@ class StockDataset(Dataset):
                 stock_data = pd.read_csv('{}{}.csv'.format(self.train_data_path, stock_code))
                 # 选择指定列
                 stock_data = pd.DataFrame(stock_data, columns=self.input_columns)
-                # 归一化数据
-                minimum = stock_data.min()
-                r = stock_data.max() - minimum
 
                 batches = len(stock_data.index) - 2 * self.data_days
                 if batches <= 0:
                     continue
                 # 数据集存入个股训练数据
                 for i in range(batches):
-                    # 清除无效数据
+                    # 清除无效数据(0)
                     if 0 in stock_data[i:i+self.data_days].values:
                         continue
-                    predict_high = stock_data.loc[data_days+i, 'high']
-                    predict_low = stock_data.loc[data_days+i, 'low']
+                    high_change = stock_data.loc[data_days+i, 'high'] / stock_data.loc[data_days+i-1, 'high']
+                    low_change = stock_data.loc[data_days+i, 'low'] / stock_data.loc[data_days+i-1, 'low']
                     predict_change = stock_data.loc[2 * data_days + i, 'close'] / stock_data.loc[data_days + i, 'close']
                     # 归一化后存入
-                    data.append({'data': ((stock_data[i:i+self.data_days]-minimum)/r).values,
-                                 'label': [predict_change, predict_low, predict_high]})
+                    data.append({'data': (stock_data[i:i+self.data_days] / stock_data.loc[i]).values,
+                                 'label': [predict_change, low_change, high_change]})
             self.data = pd.DataFrame(data)
             self.data.to_pickle('{}{}.pkl'.format(self.base_data_path, self.index_name))
         else:
@@ -84,7 +81,7 @@ class StockDataset(Dataset):
 
 class CNNModel(nn.Module):
     """类LeNet结构CNN模型"""
-    def __init__(self, data_days=10, input_size=18):
+    def __init__(self, input_size, data_days=10):
         super(CNNModel, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 3)  # 输入通道数为1，输出通道数为6
         self.conv2 = nn.Conv2d(6, 16, 3)  # 输入通道数为6，输出通道数为16
@@ -94,10 +91,10 @@ class CNNModel(nn.Module):
 
     def forward(self, x):
         x = x.view(x.size()[0], 1, x.size()[1], x.size()[2])
-        # 输入x (50, 1, 10, 18) -> conv1 (50, 6, 8, 16) -> relu
+        # 输入x (50, 1, 10, 17) -> conv1 (50, 6, 8, 15) -> relu
         x = self.conv1(x)
         x = F.relu(x)
-        # 输入x (50, 6, 8, 16) -> conv2 (50, 16, 6, 14) -> relu
+        # 输入x (50, 6, 8, 15) -> conv2 (50, 16, 6, 13) -> relu
         x = self.conv2(x)
         x = F.relu(x)
         # view函数将张量x变形成一维向量形式，总特征数不变，为全连接层做准备
@@ -119,32 +116,26 @@ class RNNModel(nn.Module):
             except KeyError:
                 raise ValueError("""非可选RNN类型,可选参数:['LSTM', 'GRU', 'RNN_TANH', 'RNN_RELU']""")
             self.rnn = nn.RNN(input_size, hidden_size, n_layers, nonlinearity=non_linearity, batch_first=True)
-        # self.fc1 = nn.Linear(hidden_size, 120)
-        # self.fc2 = nn.Linear(120, 60)
-        # self.fc3 = nn.Linear(60, 1)
+        self.fc1 = nn.Linear(hidden_size, 120)
+        self.fc2 = nn.Linear(120, 60)
+        self.fc3 = nn.Linear(60, 3)
         # self.norm = nn.BatchNorm1d(10)
-        self.fc = nn.Linear(hidden_size, 3)
+        # self.fc = nn.Linear(hidden_size, 3)
 
         self.rnn_type = rnn_type
         self.hidden_size = hidden_size
         self.n_layers = n_layers
 
     def forward(self, x):
-        # h0 = torch.zeros(self.n_layers, x.size(0), self.hidden_size).to(device)
-        # c0 = torch.zeros(self.n_layers, x.size(0), self.hidden_size).to(device)
-        # if self.rnn_type == 'LSTM':
-        #     x, _ = self.rnn(x, (h0, c0))
-        # else:
-        #     x, _ = self.rnn(x, h0)
         # x = self.norm(x)
         x, _ = self.rnn(x)
-        # x = F.relu(self.fc1(x[:, -1, :]))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
-        print(x)
-        x = torch.sigmoid(x[:, -1, :])
-        print(x)
-        x = self.fc(x)
+        x = F.relu(self.fc1(x[:, -1, :]))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # print(x)
+        # x = torch.sigmoid(x[:, -1, :])
+        # print(x)
+        # x = self.fc(x)
         return x
 
 
@@ -224,7 +215,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(4 * 512 * block.expansion, num_classes)  # 去掉池化变成4倍
+        self.linear = nn.Linear(6 * 512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -236,17 +227,11 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         x = x.view(x.size()[0], 1, x.size()[1], x.size()[2])
-        print(x)
         out = F.relu(self.bn1(self.conv1(x)))
-        print(out)
         out = self.layer1(out)
-        print(out)
         out = self.layer2(out)
-        print(out)
         out = self.layer3(out)
-        print(out)
         out = self.layer4(out)
-        print(out)
         # out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
@@ -294,20 +279,22 @@ class Prediction:
         self.batch_size = batch_size
         # 输入列
         self.input_columns = ('open', 'high', 'low', 'close', 'preclose', 'volume', 'amount',
-                              'turn', 'pctChg', 'peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ',
-                              'total_share', 'float_share', 'free_share', 'total_mv', 'circ_mv')
+                              'turn', 'peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ', 'total_share',
+                              'float_share', 'free_share', 'total_mv', 'circ_mv')
         input_size = len(self.input_columns)
 
-        # 选择cnn模型
-        self.cnn_model = CNNModel(data_days, input_size).to(device)
+        # cnn模型
+        self.cnn = CNNModel(data_days, input_size).to(device)
 
         # RNN类型 输入大小 隐层大小 隐层数
-        rnn_types = 'LSTM', 'GRU', 'RNN_TANH', 'RNN_RELU'
-        rnn_type = rnn_types[3]
-        hidden_size = 10
-        n_layers = 1
+        # rnn_types = 'LSTM', 'GRU', 'RNN_TANH', 'RNN_RELU'
+        hidden_size = 50
+        n_layers = 2
         # 初始化模型
-        self.rnn_model = RNNModel(rnn_type, input_size, hidden_size, n_layers).to(device)
+        self.lstm = RNNModel('LSTM', input_size, hidden_size, n_layers).to(device)
+        self.gru = RNNModel('GRU', input_size, hidden_size, n_layers).to(device)
+        self.rnn_tanh = RNNModel('RNN_TANH', input_size, hidden_size, n_layers).to(device)
+        self.rnn_relu = RNNModel('RNN_RELU', input_size, hidden_size, n_layers).to(device)
 
         # ResNet模型
         self.resnet18 = ResNet18().to(device)
@@ -319,110 +306,94 @@ class Prediction:
         # 使用MSE误差
         self.criterion = nn.MSELoss()
         # 使用Adam优化器 默认参数
-        self.cnn_optimizer = torch.optim.Adam(self.cnn_model.parameters())
-        self.rnn_optimizer = torch.optim.Adam(self.rnn_model.parameters())
+        self.cnn_optimizer = torch.optim.Adam(self.cnn.parameters())
+        self.lstm_optimizer = torch.optim.Adam(self.lstm.parameters())
+        self.gru_optimizer = torch.optim.Adam(self.gru.parameters())
+        self.rnn_tanh_optimizer = torch.optim.Adam(self.rnn_tanh.parameters())
+        self.rnn_relu_optimizer = torch.optim.Adam(self.rnn_relu.parameters())
         self.rn18_optimizer = torch.optim.Adam(self.resnet18.parameters())
         self.rn34_optimizer = torch.optim.Adam(self.resnet34.parameters())
         self.rn50_optimizer = torch.optim.Adam(self.resnet50.parameters())
         self.rn101_optimizer = torch.optim.Adam(self.resnet101.parameters())
         self.rn152_optimizer = torch.optim.Adam(self.resnet152.parameters())
 
-    def train_cnn(self, train_dataset, epochs=2, retrain=False):
-        if not os.path.exists('{}{}_CNN_model.pt'.format(self.base_data_path, self.index_name)):
-            retrain = True
-        if not retrain:
-            # 读取训练好的模型
-            self.cnn_model = torch.load('{}{}_CNN_model.pt'.format(self.base_data_path, self.index_name))
-            return
+    def __train(self, model_name, model, optim, train_dataset, epochs=2):
         # 生成训练数据
         train_data = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
         # 设置为训练模式
-        self.cnn_model.train()
+        model.train()
         for epoch in range(epochs):
             for data, label in tqdm(train_data):
                 # 前向传播 计算结果
-                output = self.cnn_model.forward(data)
-                # # 使label与输出维度一致
+                output = model.forward(data)
+                # 当label为一维时需要使label与输出维度一致
                 # label = label.view(label.size()[0], 1)
                 # 计算误差
                 loss = self.criterion(output, label)
                 # 清除梯度记录
-                self.cnn_optimizer.zero_grad()
+                optim.zero_grad()
                 # 误差反向传播
                 loss.backward()
                 # 优化器更新参数
-                self.cnn_optimizer.step()
-                # print('Train loss: ', loss.item())
+                optim.step()
         # 保存训练好的模型
-        torch.save(self.cnn_model, '{}{}_CNN_model.pt'.format(self.base_data_path, self.index_name))
+        torch.save(model, '{}{}.pt'.format(self.base_data_path, model_name))
 
-    def train_rnn(self, train_dataset, epochs=2, retrain=False):
-        if not os.path.exists('{}{}_RNN_model.pt'.format(self.base_data_path, self.index_name)):
+    def train_cnn(self, train_dataset, epochs=2, retrain=False):
+        if not os.path.exists('{}CNN.pt'.format(self.base_data_path)):
             retrain = True
         if not retrain:
             # 读取训练好的模型
-            self.rnn_model = torch.load('{}{}_RNN_model.pt'.format(self.base_data_path, self.index_name))
+            self.cnn = torch.load('{}CNN.pt'.format(self.base_data_path, self.index_name))
             return
-        # 生成训练数据
-        train_data = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        # 设置为训练模式
-        self.rnn_model.train()
-        for epoch in range(epochs):
-            for data, label in tqdm(train_data):
-                # 前向传播 计算结果
-                output = self.rnn_model.forward(data)
-                # print(output)
-                # # 使label与输出维度一致
-                # label = label.view(label.size()[0], 1)
-                # print(label)
-                # 计算误差
-                loss = self.criterion(output, label)
-                # 清除梯度记录
-                self.rnn_optimizer.zero_grad()
-                # 误差反向传播
-                loss.backward()
-                # # 梯度裁剪
-                # for p in self.rnn_model.parameters():
-                #     print(p.grad.norm())                 # 查看参数p的梯度
-                #     torch.nn.utils.clip_grad_norm_(self.rnn_model.parameters(), max_norm=20, norm_type=2)
-                # 优化器更新参数
-                self.rnn_optimizer.step()
-                print('Train loss: ', loss.item())
-        # 保存训练好的模型
-        torch.save(self.rnn_model, '{}{}_RNN_model.pt'.format(self.base_data_path, self.index_name))
+        self.__train('CNN', self.cnn, self.cnn_optimizer, train_dataset, epochs)
+
+    def train_lstm(self, train_dataset, epochs=2, retrain=False):
+        if not os.path.exists('{}LSTM.pt'.format(self.base_data_path)):
+            retrain = True
+        if not retrain:
+            # 读取训练好的模型
+            self.lstm = torch.load('{}LSTM.pt'.format(self.base_data_path))
+            return
+        self.__train('LSTM', self.lstm, self.lstm_optimizer, train_dataset, epochs)
+
+    def train_gru(self, train_dataset, epochs=2, retrain=False):
+        if not os.path.exists('{}GRU.pt'.format(self.base_data_path)):
+            retrain = True
+        if not retrain:
+            # 读取训练好的模型
+            self.rnn_relu = torch.load('{}GRU.pt'.format(self.base_data_path))
+            return
+        self.__train('GRU', self.gru, self.gru_optimizer, train_dataset, epochs)
+
+    def train_rnn_tanh(self, train_dataset, epochs=2, retrain=False):
+        if not os.path.exists('{}RNN_tanh.pt'.format(self.base_data_path)):
+            retrain = True
+        if not retrain:
+            # 读取训练好的模型
+            self.rnn_relu = torch.load('{}RNN_tanh.pt'.format(self.base_data_path))
+            return
+        self.__train('RNN_tanh', self.rnn_tanh, self.rnn_tanh_optimizer, train_dataset, epochs)
+
+    def train_rnn_relu(self, train_dataset, epochs=2, retrain=False):
+        if not os.path.exists('{}RNN_relu.pt'.format(self.base_data_path)):
+            retrain = True
+        if not retrain:
+            # 读取训练好的模型
+            self.rnn_relu = torch.load('{}RNN_relu.pt'.format(self.base_data_path))
+            return
+        self.__train('RNN_relu', self.rnn_relu, self.rnn_relu_optimizer, train_dataset, epochs)
 
     def train_resnet18(self, train_dataset, epochs=2, retrain=False):
-        if not os.path.exists('{}{}_rn18_model.pt'.format(self.base_data_path, self.index_name)):
+        if not os.path.exists('{}resnet18.pt'.format(self.base_data_path)):
             retrain = True
         if not retrain:
             # 读取训练好的模型
-            self.resnet18 = torch.load('{}{}_rn18_model.pt'.format(self.base_data_path, self.index_name))
+            self.resnet18 = torch.load('{}resnet18.pt'.format(self.base_data_path))
             return
-        # 生成训练数据
-        train_data = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        # 设置为训练模式
-        self.resnet18.train()
-        for epoch in range(epochs):
-            # for data, label in tqdm(train_data):
-            for data, label in train_data:
-                # 前向传播 计算结果
-                output = self.resnet18.forward(data)
-                print(output)
-                # 使label与输出维度一致
-                label = label.view(label.size()[0], 1)
-                # 计算误差
-                loss = self.criterion(output, label)
-                # 清除梯度记录
-                self.cnn_optimizer.zero_grad()
-                # 误差反向传播
-                loss.backward()
-                # 优化器更新参数
-                self.cnn_optimizer.step()
-                print('Train loss: ', loss.item())
-        # 保存训练好的模型
-        torch.save(self.resnet18, '{}{}_rn18_model.pt'.format(self.base_data_path, self.index_name))
+        self.__train('resnet18', self.resnet18, self.rn18_optimizer, train_dataset, epochs)
 
-    def predict_data(self, stock_code: str, today: tuple):
+    def __predict_data(self, stock_code: str, today: tuple):
         stock_data = pd.read_csv('{}{}.csv'.format(self.data_path, stock_code))
         # 当前日期在数据集中序号
         date_index = len(stock_data) - len(self.trading_dates) + today[1]
@@ -431,43 +402,73 @@ class Prediction:
             return 0
         # 生成预测数据
         stock_data = pd.DataFrame(stock_data, columns=self.input_columns)
-        stock_data = stock_data[date_index - self.data_days:date_index]
+        # 将0替换为上一行数据
+        stock_data = stock_data.replace(0, None)
+        # 转化为与第一行的比值
+        stock_data = stock_data[date_index - self.data_days:date_index] / stock_data.loc[date_index - self.data_days]
         stock_data = np.reshape(stock_data.values, (1, self.data_days, len(self.input_columns)))
         stock_data = torch.tensor(stock_data, dtype=torch.float32, device=device)
         return stock_data
 
-    def predict_cnn(self, stock_code: str, today: tuple):
+    def __predict(self, model, stock_code: str, today: tuple):
         # 设置为预测模式
-        self.cnn_model.eval()
-        stock_data = self.predict_data(stock_code, today)
+        model.eval()
+        stock_data = self.__predict_data(stock_code, today)
         if type(stock_data) == int:
             return 0
         with torch.no_grad():
             # 前向传播 输出结果
-            output = self.cnn_model.forward(stock_data)
+            output = model.forward(stock_data)
             return output
 
-    def predict_rnn(self, stock_code: str, today: tuple):
-        # 设置为预测模式
-        self.rnn_model.eval()
-        stock_data = self.predict_data(stock_code, today)
-        if type(stock_data) == int:
-            return 0
-        with torch.no_grad():
-            # 前向传播 输出结果
-            output = self.rnn_model.forward(stock_data)
-            return output
+    def predict_cnn(self, stock_code: str, today: tuple):
+        return self.__predict(self.cnn, stock_code, today)
+
+    def predict_lstm(self, stock_code: str, today: tuple):
+        return self.__predict(self.lstm, stock_code, today)
+
+    def predict_gru(self, stock_code: str, today: tuple):
+        return self.__predict(self.gru, stock_code, today)
+
+    def predict_rnn_tanh(self, stock_code: str, today: tuple):
+        return self.__predict(self.rnn_tanh, stock_code, today)
+
+    def predict_rnn_relu(self, stock_code: str, today: tuple):
+        return self.__predict(self.rnn_relu, stock_code, today)
+
+    def predict_resnet18(self, stock_code: str, today: tuple):
+        return self.__predict(self.resnet18, stock_code, today)
 
 
 if __name__ == '__main__':
     dataset = StockDataset(data_days=10, remake_data=False)
     print(len(dataset))
+
     prediction = Prediction(data_days=10, batch_size=50)
+
+    code = dataset.stocks_codes[0]
+    day = (prediction.trading_dates[30], 30)
+
     prediction.train_cnn(dataset, retrain=False, epochs=2)
-    out1 = prediction.predict_cnn(dataset.stocks_codes[0], (prediction.trading_dates[1], 1))
-    out2 = prediction.predict_cnn(dataset.stocks_codes[0], (prediction.trading_dates[30], 30))
-    # print(out1, out2)
-    # prediction.train_rnn(dataset, retrain=True, epochs=1)
-    # out = prediction.predict_rnn(dataset.stocks_codes[0], (prediction.trading_dates[30], 30))
-    # print(out)
-    # prediction.train_resnet18(dataset, epochs=1, retrain=True)
+    out1 = prediction.predict_cnn(code, day)
+    print(out1)
+
+    prediction.train_lstm(dataset, retrain=False, epochs=2)
+    out2 = prediction.predict_lstm(code, day)
+    print(out2)
+
+    prediction.train_gru(dataset, retrain=False, epochs=2)
+    out3 = prediction.predict_gru(code, day)
+    print(out3)
+
+    prediction.train_rnn_tanh(dataset, retrain=False, epochs=2)
+    out4 = prediction.predict_rnn_tanh(code, day)
+    print(out4)
+
+    prediction.train_rnn_relu(dataset, retrain=False, epochs=2)
+    out5 = prediction.predict_rnn_relu(code, day)
+    print(out5)
+
+    prediction.train_resnet18(dataset, retrain=False, epochs=2)
+    out6 = prediction.predict_resnet18(code, day)
+    print(out6)
